fe38285b182d646cb072027dac9048f82254e7ac
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-08-27 22:40:41 +0000

    =Change for files interface

--- files.php
+++ files.php
@@ -51,7 +51,7 @@
 	}
 	
 	//------Begin Special Display Functions--------------------------//
-			
+	
 	function printFolder( $folder ) {
 	// Prints tree structure of folders
 		if ( $_SESSION['selectedFolder'] == $folder->getID() )
@@ -59,7 +59,7 @@
 		else
 			print "<li><a href='files.php?toggleExpand=".$folder->getID()."'><img src=\"img/folder.png\" border=\"0\" alt=\"+\" title=\"Folder\" /></a>&nbsp;<a href='files.php?selectFolder=".$folder->getID()."'>".$folder->getName()."</a>\n";
 		$subfolder = $folder->getFolders();
-		if ( count($subfolder) > 0 && in_array( $folder->getID(), $_SESSION['expandFolders'] ) ) {
+		if ( count($subfolder) > 0 && (in_array( $folder->getID(), $_SESSION['expandFolders'] ) || $_SESSION['selectedFolder'] == $folder->getID() )) {
 			print "<ul class=\"filesul\">\n";
 			foreach ( $subfolder as $key => $val ) {
 				printFolder( $val );
