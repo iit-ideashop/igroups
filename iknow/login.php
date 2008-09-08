010f69cb6f129bba0e52923ef613dda25efb10d5
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-09-08 15:01:33 +0000

    =Bug fix

--- iknow/login.php
+++ iknow/login.php
@@ -11,10 +11,12 @@ ob_start();
 
 <div id="sidebar">
 	<div id="iprologo">
-		<a href="http://ipro.iit.edu/home/index.php" title="IPRO Home"><img src="../img/iprologo.png" alt="IPRO" title="IPRO" /></a>
+		<a href="http://ipro.iit.edu/home/index.php" title="IPRO 
+Home"><img src="../img/iprologo.png" alt="IPRO" title="IPRO" /></a>
 	</div>
 	<div id="igroupslogo">
-		<img src="../img/iGroupslogo.png" alt="iGroups" title="iGroups" />
+		<img src="../img/iGroupslogo.png" alt="iGroups" 
+title="iGroups" />
 	</div>
 	
 	<div id="loginform">
@@ -26,10 +28,12 @@ ob_start();
 ?>
 			<script type="text/javascript">
 			<!--
-					window.location.href="index.php";
+				window.location.href="../index.php";
 			//-->
 			</script>
 <?php
+ob_end_flush();
+		}
 	
 		if ( isset ( $_SESSION['loginError'] ) )
 			print "<strong>Invalid username or password.</strong><br />";
@@ -46,9 +50,9 @@ ob_start();
 		</fieldset></form>
 	</div>
 	<p>
-	<a href="../forgotpassword.php" title="Forgotten password">Forgot password?</a><br />
+	<a href="../forgotpassword.php" title="Forgotten 
+password">Forgot password?</a><br />
 	<a href="../UM_iGROUPS.pdf" title="User manual">iGROUPS User Manual</a><br />
 	<a href="../needhelp.php" title="Help">Need help?</a>
 	</p>
-	<div id="sidebar-bottom"></div>
 </div>
