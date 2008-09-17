6d2f4946fcfcf8dbad0d50312204ad507a00a490
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-09-17 00:10:22 +0000

    =On the fly login

--- email.php
+++ email.php
@@ -1,9 +1,46 @@
 <?php
-	include_once("checklogin.php");
+	include_once("checklogingroupless.php");
 	include_once( "classes/group.php" );
 	include_once( "classes/category.php" );
 	include_once( "classes/email.php" );
+	
+	if(isset($_GET['replyid']))
+	{
+		$email = new Email($_GET['replyid'], $db);
+		$_SESSION['selectedGroup'] = $email->getGroupID();
+		$_SESSION['selectedGroupType'] = $email->getGroupType();
+		$_SESSION['selectedSemester'] = $email->getSemester();
+	}
+	
+	if ( isset($_SESSION['selectedGroup']) && isset($_SESSION['selectedGroupType']) && isset($_SESSION['selectedSemester']) )
+	{
+		$currentGroup = new Group( $_SESSION['selectedGroup'], $_SESSION['selectedGroupType'], $_SESSION['selectedSemester'], $db );
+		if(!$currentGroup->isGroupMember($currentUser))
+		{
+			unset($_SESSION['selectedGroup']);
+			unset($_SESSION['selectedGroupType']);
+			unset($_SESSION['selectedSemester']);
+			setcookie('selectedGroup', '', time()-60);
+			die("You are not a member of this group.");
+		}
+	}
+	else if(isset($_COOKIE['selectedGroup']))
+	{
+		$group = explode(",", $_COOKIE['selectedGroup']);
+		$currentGroup = new Group( $group[0],$group[1], $group[2], $db );
+		if(!$currentGroup->isGroupMember($currentUser))
+		{
+			setcookie('selectedGroup', '', time()-60);
+			die("You are not a member of this group.");
+		}
+		$_SESSION['selectedGroup'] = $group[0];
+		$_SESSION['selectedGroupType'] = $group[1];
+		$_SESSION['selectedSemester'] = $group[2];
 		
+	}
+	else
+		die("You have not selected a valid group.");
+	
 	if ( isset( $_GET['selectCategory'] ) ) {
 		$_SESSION['selectedCategory'] = $_GET['selectCategory'];
 	}
