5e671bb998f964a5f848ca9617c70cdf63b5b151
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-09-17 00:22:00 +0000

    =On the fly login

--- menu.php
+++ menu.php
@@ -80,8 +80,7 @@ ob_start();
 			$_SESSION['selectedGroup'] = $temp[0];
 			$_SESSION['selectedGroupType'] = $temp[1];
 			$_SESSION['selectedSemester'] = $temp[2];
-			if(isset($_COOKIE['userID']))
-				setcookie('selectedGroup', $string, time()+60*60*24*365);
+			setcookie('selectedGroup', $string, time()+60*60*24*365);
 		}
 		unset( $_SESSION['selectedFolder'] );
 		unset( $_SESSION['selectedSpecial'] );
