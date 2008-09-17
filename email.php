63008ab76a34bbcaf6d9cd9170f695a770de89fc
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-09-17 20:26:20 +0000

    =Spell check

--- email.php
+++ email.php
@@ -132,21 +132,17 @@
 * Visit http://www.dynamicdrive.com/ for full source code
 ***********************************************/
 </script>
-
 	<script type="text/javascript">
 	//<![CDATA[
-	
-		function spellcheck( id ) {
+		function spellcheck(id) {
 			var spellwin=dhtmlwindow.open('spellbox', 'ajax', 'spell.php', 'Spell Check', 'width=600px,height=400px,left=300px,top=100px,resize=1,scrolling=1');
-			spellwin.onclose=returnChecked(id);
-			textval = document.getElementById( id ).value;
-			document.getElementById( 'spelltext' ).value = textval;
-		}
-
-		function returnChecked( id ) {
-			textval = document.getElementById( 'spelltext' ).value;
-			document.getElementById( id ).value = textval;
-			spellwin.close();
+			spellwin.onclose=function() {
+				textval = document.getElementById('spelltext').value;
+				document.getElementById(id).value = textval;
+				return true;
+			}
+			textval = document.getElementById(id).value;
+			document.getElementById('spelltext').value = textval;
 		}
 
 		function toggleToDisplay() {
