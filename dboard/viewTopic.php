c3aba6691ccdb3abbfb7a03d2cd7d5f7484c600d
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-09-22 15:52:59 +0000

    =Bug fixes

--- dboard/viewTopic.php
+++ dboard/viewTopic.php
@@ -44,7 +44,6 @@
 	$allThreads = $currentTopic->getThreads();
 	setcookie('topic', $currentTopic->getID(), time()+60*60*6);
 	setcookie('topicLink', $link, time()+60*60*6); 
-	setcookie('topicName', $topicName, time()+60*60*6);
 	
 	if (isset($currentGroup)) {
 		setcookie('groupType', $currentGroup->getType(), time()+60*60*6);
@@ -131,7 +130,7 @@
 		$topicName = $currentGroup->getName() . " Discussion";
 	}
 
-	setcookie('topicName', $topicName, time()*60*60*6);
+	setcookie('topicName', $topicName, time()+60*60*6);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- This web-based application is Copyrighted &copy; 2008 Interprofessional Projects Program, Illinois Institute of Technology -->
