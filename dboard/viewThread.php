041407b1b5489107e38d8058ee285daeb4fdd37e
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-10-01 22:00:51 +0000

    =DB bug fix

--- dboard/viewThread.php
+++ dboard/viewThread.php
@@ -148,7 +148,7 @@
 		$globaltext = "&amp;topicID=".$_GET['topic']."&amp;global=true";
 	else
 		$globaltext = "&amp;topicID=".$_GET['topic'];
-	$threadtext = "&amp;thread=".$_GET['thread'];
+	$threadtext = "&amp;thread=".$_GET['id'];
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- This web-based application is Copyrighted &copy; 2008 Interprofessional Projects Program, Illinois Institute of Technology -->
