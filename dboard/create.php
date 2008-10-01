53776ca404c440b4319c0fe29c6d3c62ce0ece7d
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-10-01 22:02:14 +0000

    =DB bug fix

--- dboard/create.php
+++ dboard/create.php
@@ -19,7 +19,7 @@
 	
 	if(isset($_GET['thread']) && is_numeric($_GET['thread']))
 		setcookie('thread', $_GET['thread'], time()+60*60*6);
-	else
+	else if ($_GET['mode'] == 'post')
 		die("No thread selected");
 
 	if (isset($_GET['mode']) && $_GET['mode'] == 'thread') {
