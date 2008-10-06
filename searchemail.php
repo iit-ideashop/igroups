103b975318f46aa5f8277d23cc4d2951ad785ebe
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-10-06 18:43:03 +0000

    =Search emails

--- searchemail.php
+++ searchemail.php
@@ -13,7 +13,7 @@
 	if(isset($_POST['bodySearch']))
 		$searchParams .= "and match(sBody) against('".mysql_real_escape_string($_POST['bodySearch'])."' in boolean mode) ";
 	
-	$query = $db->igroupsQuery("select iID from Emails where iGroupID=".$currentGroup->getID()." and iGroupType=".$currentGroup->getType()." and iSemesterID=".$currentGroup->getSemester()." $searchParams");
+	$query = $db->igroupsQuery("select iID from Emails where iGroupID=".$currentGroup->getID()." and iGroupType=".$currentGroup->getType()." and iSemesterID=".$currentGroup->getSemester()."  $searchParams");
 	$emails = array();
 	while($row = mysql_fetch_row($query))
 		$emails[] = new Email($row[0], $db);
