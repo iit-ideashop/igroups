275605c7db1d7c5b9a8132b4d21b3b3e8b5a7546
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-12-03 22:10:31 +0000

    =Bugfix

--- iknow/appearance.php
+++ iknow/appearance.php
@@ -11,7 +11,7 @@
 		die('No database connection');
 	$skin = $lynyrdskynyrd[0];
 	$altskins = array();
-	$lynyrdskynyrd = $db->igroupsQuery('select sName from Skins where iID not in (select iSkin from People where iID='.$currentUser->getID().') and bPublic=1';
+	$lynyrdskynyrd = $db->igroupsQuery('select sName from Skins where iID not in (select iSkin from People where iID='.$currentUser->getID().') and bPublic=1');
 	while($row = mysql_fetch_row($lynyrdskynyrd))
 		$altskins[] = $row[0];
 ?>
