3565d1f233bdce969262cceb447d2ad2c6920587
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-12-03 22:35:27 +0000

    =Bugfix

--- calendar.php
+++ calendar.php
@@ -45,14 +45,14 @@ foreach($altskins as $altskin)
 
 		function viewEvent( name, desc, date ) {
 			document.getElementById('viewname').innerHTML=name;
-			desc = desc.replace("&lt;a href", "<a onclick=\"window.open(this.href); return false;\" href");
-			desc = desc.replace("&lt;A HREF", "<a onclick=\"window.open(this.href); return false;\" href");
-			desc = desc.replace("<a href", "<a onclick=\"window.open(this.href); return false;\" href");
-			desc = desc.replace("<A HREF", "<a onclick=\"window.open(this.href); return false;\" href");
-			desc = desc.replace("&lt;/a", "</a");
-			desc = desc.replace("&gt;", ">");
-			desc = desc.replace("&amp;quot;", "\"");
-			desc = desc.replace("&quot;", "\"");
+			desc = desc.replace(/&lt;a href/g, "<a onclick=\"window.open(this.href); return false;\" href");
+			desc = desc.replace(/&lt;A HREF/g, "<a onclick=\"window.open(this.href); return false;\" href");
+			desc = desc.replace(/<a href/g, "<a onclick=\"window.open(this.href); return false;\" href");
+			desc = desc.replace(/<A HREF/g, "<a onclick=\"window.open(this.href); return false;\" href");
+			desc = desc.replace(/&lt;\/a/g, "</a");
+			desc = desc.replace(/&gt;/g, ">");
+			desc = desc.replace(/&amp;quot;/g, "\"");
+			desc = desc.replace(/&quot;/g, "\"");
 			document.getElementById('viewdesc').innerHTML=desc;
 			document.getElementById('viewdate').innerHTML=date;
 		}
