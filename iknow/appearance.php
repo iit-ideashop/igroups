93b506cb002c6ce668b389257ad393be1668d29e
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-12-03 22:13:22 +0000

    =Bugfix

--- iknow/appearance.php
+++ iknow/appearance.php
@@ -3,15 +3,22 @@
 	{
 		$lynyrdskynyrd = mysql_fetch_row($db->igroupsQuery('select sName from Skins where iID in (select iSkin from People where iID='.$currentUser->getID().')'));
 		if(!$lynyrdskynyrd)
+		{
 			$lynyrdskynyrd = mysql_fetch_row($db->igroupsQuery('select sName from Skins where bDefault=1'));
+			$alts = $db->igroupsQuery('select sName from Skins where bDefault=0 and bPublic=1');
+		}
+		else
+			$alts = $db->igroupsQuery('select sName from Skins where iID not in (select iSkin from People where iID='.$currentUser->getID().') and bPublic=1');
 	}
 	else if(isset($db))
+	{
 		$lynyrdskynyrd = mysql_fetch_row($db->igroupsQuery('select sName from Skins where bDefault=1'));
+		$alts = $db->igroupsQuery('select sName from Skins where bDefault=0 and bPublic=1');
+	}
 	else
 		die('No database connection');
 	$skin = $lynyrdskynyrd[0];
 	$altskins = array();
-	$lynyrdskynyrd = $db->igroupsQuery('select sName from Skins where iID not in (select iSkin from People where iID='.$currentUser->getID().') and bPublic=1');
-	while($row = mysql_fetch_row($lynyrdskynyrd))
+	while($row = mysql_fetch_row($alts))
 		$altskins[] = $row[0];
 ?>
