12a26687b79001108268f1e436c68d08501055b2
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2008-12-03 22:43:35 +0000

    =Bugfix

--- viewtimesheets.php
+++ viewtimesheets.php
@@ -194,19 +194,12 @@ foreach($altskins as $altskin)
 <!-- This web-based application is Copyrighted &copy; 2008 Interprofessional Projects Program, Illinois Institute of Technology -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
 <title>iGroups - View Timesheet Reports</title>
-<?php require("appearance.php"); ?>
-	<style type="text/css">		
-		table {
-			text-align:left;
-		}
-		
-		thead {
-			background-color:#DDD;
-		}
-		tfoot {
-			background-color:#DDD;
-		}
-	</style>
+<?php
+require("appearance.php");
+echo "<link rel=\"stylesheet\" href=\"skins/$skin/timesheet.css\" type=\"text/css\" title=\"$skin\" />\n";
+foreach($altskins as $altskin)
+	echo "<link rel=\"alternate stylesheet\" href=\"skins/$altskin/timesheet.css\" type=\"text/css\" title=\"$altskin\" />\n";
+?>
 </head>
 <body>
 <?php
