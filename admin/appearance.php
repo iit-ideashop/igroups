de6d16b7f1c484208a451fdfde82586cf1e7d690
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-02-25 07:03:17 +0000

    =Bugfix

--- admin/appearance.php
+++ admin/appearance.php
@@ -15,7 +15,7 @@
 <!-- This web-based application is Copyrighted &copy; 2008 Interprofessional Projects Program, Illinois Institute of Technology -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
 <?php
-require("../iknow/appearance.php");
+require_once("../iknow/appearance.php");
 echo "<link rel=\"stylesheet\" href=\"../skins/$skin/default.css\" type=\"text/css\" title=\"$skin\" />\n";
 foreach($altskins as $altskin)
 	echo "<link rel=\"alternate stylesheet\" href=\"../skins/$altskin/default.css\" type=\"text/css\" title=\"$altskin\" />\n";
