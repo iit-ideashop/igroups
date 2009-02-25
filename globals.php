3b2e67710c433a3a93a640f243b7b8e735307d58
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-02-25 07:09:59 +0000

    =Bugfix

--- globals.php
+++ globals.php
@@ -22,9 +22,9 @@
 			else
 				header('HTTP/1.1 500 Internal Server Error');
 			echo<<<EOF
-			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-			<!-- This web-based application is Copyrighted &copy; 2009 Interprofessional Projects Program, Illinois Institute of Technology -->
-			<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!-- This web-based application is Copyrighted &copy; 2009 Interprofessional Projects Program, Illinois Institute of Technology -->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"><head>
 EOF;
 			require('appearance.php');
 			echo "<link rel=\"stylesheet\" href=\"skins/$skin/tasks.css\" type=\"text/css\" title=\"$skin\" />\n";
