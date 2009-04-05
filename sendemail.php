5bcd9710f51d5685ae141459cb054fefbb5c1660
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-04-05 23:53:10 +0000

    =Signature

--- sendemail.php
+++ sendemail.php
@@ -20,7 +20,8 @@
 	unset($query);
 	$sig = $arr[0];
 	unset($arr);
-	$sig = "\n--\n$sig";
+	if($sig)
+		$sig = "\n--\n$sig";
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- This web-based application is Copyrighted &copy; 2008 Interprofessional Projects Program, Illinois Institute of Technology -->
