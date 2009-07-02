359e46b840c513f2e472dbf07949c9eec00168b2
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-07-02 03:15:22 +0000

    =Nugget verification

--- classes/nugget.php
+++ classes/nugget.php
@@ -73,7 +73,7 @@ if( !class_exists( "Nugget" ) ) {
 		}
 		
 		function verify($admin) {
-			$g = new Group($this->group, $this->db);
+			$g = new Group($this->group, 0, $this->semester, $this->db);
 			if($admin->isGroupAdministrator($g))
 				return $db->igroupsQuery("update iGroupsNuggets set iVerified={$admin->getID()}, dVerified=NOW() WHERE iNuggetID = {$this->id}");
 			else
