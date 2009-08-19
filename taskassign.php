b9a1435e991f35a2c74716b1f7d37017752679a7
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-08-19 17:53:32 +0000

    =Email notifications

--- taskassign.php
+++ taskassign.php
@@ -47,7 +47,7 @@
 				if(!array_key_exists($id, $oldassigned) && $person->receivesNotifications())
 				{
 					$added = $this->isAssigned($person) ? 'assigned to' : 'unassigned from';
-					$msg = "This is an auto-generated $appname notification to let you know that you have been $added a task. Task information is below.\n\n";
+					$msg = "This is an auto-generated $appname notification to let you know that you have been assigned to a task. Task information is below.\n\n";
 					$msg .= "Group: {$task->team->getName()}\nTask Name:{$task->getName()}\nURL: $appurl/taskview.php?taskid={$task->getID()}\n\n";
 					$msg .= "--- $appname System Auto-Generated Massage\n\n";
 					$msg .= "To stop receiving task assignment notifications, visit $appurl/contactinfo.php";
@@ -57,8 +57,15 @@
 			}
 			foreach($oldassigned as $id => $person)
 			{
-				if(!array_key_exists($id, $newassigned))
-					$task->informAssignedPerson($person);
+				if(!array_key_exists($id, $newassigned) && $person->receivesNotifications())
+				{
+					$msg = "This is an auto-generated $appname notification to let you know that you have been unassigned from a task. Task information is below.\n\n";
+					$msg .= "Group: {$task->team->getName()}\nTask Name:{$task->getName()}\nURL: $appurl/taskview.php?taskid={$task->getID()}\n\n";
+					$msg .= "--- $appname System Auto-Generated Massage\n\n";
+					$msg .= "To stop receiving task assignment notifications, visit $appurl/contactinfo.php";
+			
+					mail($person->getEmail(), "[$appname] Task Assignment", $msg, "From: $appname Support <$contactemail>");
+				}
 			}
 			header('Location: taskview.php?taskid='.$task->getID());
 		}
