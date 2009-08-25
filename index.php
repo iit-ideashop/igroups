15727d2f22b8555a3182e554012447e02f6622b4
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-08-25 00:19:49 +0000

    =Trunk: CSS fix

--- index.php
+++ index.php
@@ -69,7 +69,7 @@
 	$title = htmlspecialchars($rand['sTitle']);
 	$desc = htmlspecialchars($rand['sDesc']);
 	echo "<div id=\"left\"><img src=\"http://ipro.iit.edu/home/images/students1/photos/$randid.$ext\" alt=\"Random photo\" id=\"randphoto\" /><br />\n";
-	echo "<span id=\"randtitle\">$title</span><br />\n<span id=\"randdesc\">$desc</span>";
+	echo "<div id=\"randtitle\">$title</div><br />\n<div id=\"randdesc\">$desc</div>";
 	echo "</div>\n";
 	
 	$heading = $numAnnounce ? $firstAnnouncement->getHeadingHTML() : "Welcome to $appname";
