51a5322ef09ab98d1ab2afb669c1e4f975201eb5
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-10-28 19:58:08 +0000

    =Fix error

--- globals.php
+++ globals.php
@@ -50,6 +50,42 @@
 			echo "</ul>\n";
 			die('</div></body></html>');
 		}
+		
+		function notAdmin()
+		{
+			global $appname, $appurl, $contactemail, $db, $currentUser, $currentGroup;
+			$responses = array(400 => 'Bad Request', 401 => 'Authorization Required', 403 => 'Forbidden', 500 => 'Internal Server Error');
+			$response = 403;
+			$title = 'Credentials Required';
+			$desc = 'You are not an administrator';
+			header("HTTP/1.1 $response {$responses[$response]}");
+			require('../doctype.php');
+			require('../appearance.php');
+			echo "<link rel=\"stylesheet\" href=\"$appurl/skins/$skin/default.css\" type=\"text/css\" title=\"$skin\" />\n";
+			foreach($altskins as $altskin)
+				echo "<link rel=\"alternate stylesheet\" href=\"$appurl/skins/$altskin/default.css\" type=\"text/css\" title=\"$altskin\" />\n";
+			echo "<title>$appname - Fatal Error</title>\n";
+			echo "</head><body>\n";
+			require('sidebar.php');
+			echo "<div id=\"content\">\n";
+			echo "<h1>Fatal Error - $response {$responses[$response]}</h1>\n";
+			echo "<h2>$title</h2>\n";
+			echo "<p>$appname cannot perform the operation you requested, for the following reason: $desc</p>\n";
+			echo "<p>If you wish to receive support for this problem, please email <a href=\"mailto:$contactemail\">$contactemail</a> with the information below, along with any additional information that you feel is relevant.</p>\n";
+			echo "<ul>\n";
+			echo "<li><b>Requested URI</b>: {$_SERVER['REQUEST_URI']}</li>\n";
+			echo "<li><b>Referring page</b>: {$_SERVER['HTTP_REFERER']}</li>\n";
+			echo "<li><b>User Agent</b>: {$_SERVER['HTTP_USER_AGENT']}</li>\n";
+			if($currentUser)
+				echo "<li><b>User ID</b>: {$currentUser->getID()}</li>\n";
+			if($currentGroup)
+				echo "<li><b>Group ID</b>: {$currentGroup->getID()}</li>\n";
+			echo "<li><b>Error title:</b> $title</li>\n";
+			echo "<li><b>Error description:</b> $desc</li>\n";
+			echo "<li><b>HTTP response code:</b> $response</li>\n";
+			echo "</ul>\n";
+			die('</div></body></html>');
+		}
 	}
 	
 	if(!function_exists('printTR'))
