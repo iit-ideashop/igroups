d1fed524655ca7a7b133113a9d370c8211f60a2e
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-12-19 03:49:51 +0000

    =Made more changes to script

--- ChangeLocation.js
+++ ChangeLocation.js
@@ -1,9 +1,15 @@
 // The following function is placed in the HTML head
 function gotoSemesterUrl()
    {
-   var selectedSemester = document.semesterlist.selectedIndex;
-   var url_address = document.semesterlist.options[selectedSemester].value;
+   //get sleected index
+   var selectedSemester = document.getElementById('semesterlist').selectedIndex;
+
+   //get value at selected index
+   var url_address = location.href + document.getElementById('semesterlist').options[selectedSemester].value;
+    
+   //got to location at index
    window.location.href = url_address;
+
    }
 
 
