5861fde50901a80c272ee5a8dc16e3d4b1645c39
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-12-20 02:22:19 +0000

    =added html layout files

---
+++ htmlcontentfoot.php
@@ -0,0 +1,17 @@
+</div><!-- end mainContent -->
+<?php
+	require('sidebar.php');
+?>
+</div>
+		<!-- end contentWrapper -->
+		
+		<!--begin footer -->
+		<div id="footer">
+			<!-- start copyright statement -->
+			<p id="copyright">iGroups &copy; 2009 &nbsp;<a href="http://www.ipro.iit.edu">Interprofessional Projects Program</a> </p> <p id="department"> <a href="http://iit.edu">Illinois Institute of Technology</a></p>
+			<!-- end copyright statement -->
+		</div>
+		<!-- end footer -->
+		
+	</div> 
+	<!-- end main container -->
---
+++ htmlhead.php
@@ -0,0 +1,39 @@
+<!-- this is the head for all pages, revised design -->	
+<!-- begin main container -->
+  <div id="mainContainer">
+			<!-- start main header -->
+			<div id="mainheader">
+					<!-- iGroups logo -->
+					<img id="igroupsLogo" src="skins/Red/img/iGroupslogo.png" alt="iGroups Logo" />
+					<!-- end logo -->
+						
+					<!-- start container for both external and internal links -->
+					<div class="links">
+			    	
+							<!-- external links -->	
+							<ul id="externallinks">
+								<li><a href="http://sloth.iit.edu/~iproadmin/peerreview/">Peer Review</a></li>
+								<li><a href="http://ipro.iit.edu">IPRO Website</a></li>
+								<li><a href="login.php?logout=true" title="Logout">Logout</a></li>
+							</ul>
+							<!-- end external links -->
+							
+							<!-- internal links/main navigation --> 
+							<ul id="mainNavigation">
+						 		<li><a href="index.php" id="home">Home</a></li>
+								<li><a href="contactinfo.php">My Profile</a></li>
+								<li><a href="iknow/main.php">Browse Nuggets</a>&nbsp;</li>
+								<li><a href="usernuggets.php">Groups' Nuggets</a></li>
+								<li><a href="help/index.php">Help</a></li>
+								<li><a href="needhelp.php">Contact Us</a></li>
+							</ul>
+							<!-- end internal links -->
+						</div>
+						<!-- end internal/external links container -->
+			
+	 			</div>
+				<!-- end main header -->
+
+				<div id="contentWrapper">			
+				<!-- begin main content -->
+		   	<div id="mainContent" >
