9a8cd14c1fcb238ddaa7aefbf0f58d6043e357e0
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2009-12-28 02:58:14 +0000

    =removed topbanner form files page

--- files.php
+++ files.php
@@ -446,8 +446,6 @@
 	 /**** begin html head *****/
    require('htmlhead.php'); //starts main container
   /****end html head content ****/	
-
-	echo "<div id=\"topbanner\">{$currentGroup->getName()}</div>\n";
 ?>
   <div id="container"> 
   
