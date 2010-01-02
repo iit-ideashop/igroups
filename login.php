5a43f82d865fbc4186535466f50db70b2e2aa7b2
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-02 21:04:07 +0000

    =edited index css and index.php

--- login.php
+++ login.php
@@ -20,6 +20,8 @@
 <?php
 	}
 ?>
+    <a href="index.php"><?php echo $appname; ?> Home</a>
+    <a href="iknow/main.php">iKnow/iGroups Guest Access</a>
 <div id="loginform">
 <?php
 	if(isset($_SESSION['loginError']))
@@ -28,9 +30,6 @@
 		unset($_SESSION['loginError']);
 	}
 ?>
-	<a href="index.php"><?php echo $appname; ?> Home</a><br /><br />
-	<a href="iknow/main.php">iKnow/iGroups Guest Access</a><br /><br />
-
 	<form method="post" action="menu.php?loggingin=true"><fieldset>
 		<label for="username">User name:</label><input name="username" id="username" type="text" /><br />
 		<label for="password">Password:</label><input name="password" id="password" type="password" /><br />
@@ -39,12 +38,12 @@
 		<input type="submit" name="login" value="Login" />
 	</fieldset>
 </form>
-</div>
-
 <p>
 <a href="http://sloth.iit.edu/~iproadmin/userpassword.php?reset=1" title="Forgotten password">Forgot password?</a><br />
 <a href="help/index.php" title="Help Center">Help Center</a><br />
 <a href="needhelp.php" title="Contact Us">Contact Us</a>
 </p>
+</div>
+
 <hr />
 <p>Return to <a href="http://sloth.iit.edu/~iproadmin/peerreview/">Peer Review</a> &#183; <a href="http://ipro.iit.edu">IPRO Website</a></p>
