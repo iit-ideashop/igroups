800f74fb39ddacf48f9a83b3ef1b73bf28495ec5
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-04 22:20:41 +0000

    =modified index in help

--- help/sidebar.php
+++ help/sidebar.php
@@ -1,6 +1,6 @@
 <?php
 	include_once('../globals.php');
-	echo "<div id=\"unisidebar\">\n";
+	echo "<div id=\"uniSidebar\">\n";
 	if (isset($_SESSION['userID']) && !$_GET['logout'])
 		require('../menu.php');
 	else
