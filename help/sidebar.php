5a25b0b12db7b1363719daf16f3438dac9272a3f
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-04 22:17:22 +0000

    =modified sidebar in help

--- help/sidebar.php
+++ help/sidebar.php
@@ -2,9 +2,9 @@
 	include_once('../globals.php');
 	echo "<div id=\"uniSidebar\">\n";
 	if (isset($_SESSION['userID']) && !$_GET['logout'])
-		require('../dboard/menu.php');
+		require('../menu.php');
 	else
-		require('../dboard/login.php');
+		require('../login.php');
 	echo "</div>\n";
 	if(isset($message))
 		echo "<div id=\"messageBox\">$message</div>";
