1f79cb0eba4423565f55d1d352299834d032ae7c
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-04 20:59:04 +0000

    =added clear to main template in htmlfootercontent

--- dboard/sidebar.php
+++ dboard/sidebar.php
@@ -2,9 +2,9 @@
 	include_once('../globals.php');
 	echo "<div id=\"unisidebar\">\n<div id=\"igroupslogo\"></div>\n";
 	if (isset($_SESSION['userID']) && !$_GET['logout'])
-		require('menu.php');
+		require('../menu.php');
 	else
-		require('login.php');
+		require('../login.php');
 	echo "</div>\n";
 	if(isset($message))
 		echo "<div id=\"messageBox\">$message</div>";
