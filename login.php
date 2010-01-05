113317b6f8c8388c49a329f9d6dd4783287aea8b
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-05 01:29:24 +0000

    =replaced logout page

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<!-- THis is the page shown when a users session expires-->
 <?php
 	include_once('globals.php');
 	include_once('classes/db.php');
@@ -14,15 +13,12 @@
 ?>
 <script type="text/javascript">
 //<![CDATA[
-	//window.location.href = "index.php";
+	window.location.href = "index.php";
 //]]>
 </script>
 <?php
 	}
 ?>
-   <a href="index.php"><?php echo $appname; ?> Home</a>
-   <br /><br />
-  <a href="iknow/main.php">iKnow/iGroups Guest Access</a>
 <div id="loginform">
 <?php
 	if(isset($_SESSION['loginError']))
@@ -31,21 +27,20 @@
 		unset($_SESSION['loginError']);
 	}
 ?>
-  
+	<a href="index.php"><?php echo $appname; ?> Home</a><br /><br />
+	<a href="iknow/main.php">iKnow/iGroups Guest Access</a><br /><br />
 	<form method="post" action="menu.php?loggingin=true"><fieldset>
 		<label for="username">User name:</label><input name="username" id="username" type="text" /><br />
 		<label for="password">Password:</label><input name="password" id="password" type="password" /><br />
 		<input type="hidden" name="logform" value="true" />
 		<input type="checkbox" name="remember" id="remember" value="true" />&nbsp;<label for="remember">Remember Me</label><br />
-		<button type="submit" name="login"> Login </button>
-	</fieldset>
-</form>
+		<input type="submit" name="login" value="Login" />
+	</fieldset></form>
+</div>
 <p>
 <a href="http://sloth.iit.edu/~iproadmin/userpassword.php?reset=1" title="Forgotten password">Forgot password?</a><br />
 <a href="help/index.php" title="Help Center">Help Center</a><br />
 <a href="needhelp.php" title="Contact Us">Contact Us</a>
 </p>
-</div>
-
 <hr />
 <p>Return to <a href="http://sloth.iit.edu/~iproadmin/peerreview/">Peer Review</a> &#183; <a href="http://ipro.iit.edu">IPRO Website</a></p>
