b8f43bf5ba20baedce4da25c5f8fefb73350d6f7
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-06 01:44:59 +0000

    =UI change to menu annd side bar in admin

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,11 +1,12 @@
 <?php
-	include_once('../globals.php');
-	echo "<div id=\"sidebar\">\n<div id=\"igroupslogo\"></div>\n";
-	if(isset($_SESSION['userID']) && !$_GET['logout'])
+	include_once('globals.php');
+	
+	echo "<div id=\"uniSidebar\">\n";
+	if (isset($_SESSION['userID']) && !$_GET['logout'])
 		require('menu.php');
 	else
 		require('login.php');
 	echo "</div>\n";
 	if(isset($message))
-		echo "<div id=\"messageBox\">$message</div>";
+		echo "<div id=\"messageBox\">$message</div>\n";
 ?>
