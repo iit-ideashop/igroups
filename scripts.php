bd0da52251d587777aee152dc48d6f2cf3a0fc40
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-13 04:38:04 +0000

    =added jquery code for date picker

--- scripts.php
+++ scripts.php
@@ -9,6 +9,6 @@
     $(document).ready(function () {
 		$("#datepick").datepicker({ dateFormat: 'yy-mm-dd', showOn: 'focus' });
 		
-	})
+	});
 <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 <script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/jquery-ui.min.js></script>-->
