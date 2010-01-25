4882eee8eb68af892549c13876ad907669fcb0ef
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-25 04:14:35 +0000

    =updated all htmlheads

--- menu.php
+++ menu.php
@@ -234,14 +234,11 @@
 
 		if(isset($igroups))
 		{
-			if(in_array('igroups', $_SESSION['expandSemesters']))
-			{
 				@ksort($igroups);
 				foreach($igroups as $key => $group)
 				{
         	echo "<option value=\"".getLink($group)."\">".$group->getName().", ".$semester->getName()."</option>\n";
 				}
-			}
 		}
 
      echo "</select><br />";
