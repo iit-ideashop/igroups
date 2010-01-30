418973df313ed84d4b6d78e01287c38bf338bc78
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-30 18:50:38 +0000

    =trying to fix group issue

--- menu.php
+++ menu.php
@@ -106,6 +106,7 @@
 			if (!isset($_SESSION['selectionMade']))
 			{	
 					$activegroup = $currentUser->getActiveGroup();
+          echo "active groups is: {$activegroups}"
 					if(!empty($activegroup))
 					{
 						$_SESSION['activateDefaultMenu'] = 1;
