ad5f718fbf9709bd63327f2bc88ef5cecd5d9cff
iproadmin <iproadmin@fd1e7e74-5f54-0410-9e2d-f9481cb802d2>
2010-01-30 20:56:53 +0000

    =trying to group issue fied byreverting links in menu page

--- menu.php
+++ menu.php
@@ -108,11 +108,16 @@
 					if(!empty($activegroups))
 					{
 						$defaultactivegroup = $activegroups[0];
-
+						pri
 						$_SESSION['activateDefaultMenu'] = 1;
 						$_SESSION['selectedGroup'] = $defaultactivegroup->getID();
 						$_SESSION['selectedGroupType'] = $defaultactivegroup->getType();
 						$_SESSION['selectedSemester'] = $defaultactivegroup->getSemester();
+
+						unset($_SESSION['selectedFolder']);
+						unset($_SESSION['selectedSpecial']);
+						unset($_SESSION['expandFolders']);
+						unset($_SESSION['selectedCategory']);
 					}
 			}
 			ob_end_flush();
